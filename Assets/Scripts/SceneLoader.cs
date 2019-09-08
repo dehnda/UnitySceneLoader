c63684bd1eae334a7646c7ce9dfe960847a33a50
daniel nimmervoll <daniel.nimmervoll@gmail.com>
2019-09-08 15:17:36 +0200

    [FEAT] basic sceneLoaderClass implemented

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+   public GameObject loadingScreen;
+   public Slider slider;
+   public Text progressText;
+      
+   public void LoadScene(int sceneIndex)
+   {
+      StartCoroutine(LoadAsynchronously(sceneIndex));
+   }
+
+   IEnumerator LoadAsynchronously(int sceneIndex)
+   {
+      AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+      loadingScreen.SetActive(true);
+      
+      while (!operation.isDone)
+      {
+         float progress = Mathf.Clamp01(operation.progress / .9f);
+
+         progressText.text = ((int) (progress * 100)).ToString() + " %";
+         slider.value = progress;
+
+            yield return null;
+      }
+   }
+}
