9884955a3aea1cdf007c4968e90fa16333734433
daniel nimmervoll <daniel.nimmervoll@gmail.com>
2019-09-17 18:49:11 +0200

    add delay script for loading process

---
+++ Assets/Scripts/loadDelay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadDelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10000000; i++)
+        {
+            var tempObject = new GameObject();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
