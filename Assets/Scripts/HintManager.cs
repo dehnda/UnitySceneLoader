489cf7ac8138f7bc8fb86bd821ec75fa9a78f761
Daniel Nimmervoll <daniel.nimmervoll@swarovski.com>
2019-10-31 15:49:14 +0100

    [FEAT] add hintmanager to scenemanager
    creates demo hints if no ressource file exists, else
    it loads hints.json from resourcefolder

---
+++ Assets/Scripts/HintManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class HintManager : MonoBehaviour
+{
+    private Hints hints;
+
+    [SerializeField]
+    private string Filename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hints = new Hints();
+
+        string path = Application.dataPath + "/Resources/" + Filename;
+
+        if (!File.Exists(path))
+        {
+            var json = CreateDemoHints();
+            WriteToFile(json, path);
+        }
+        else
+        {
+            var json = ReadFromFile(Filename);
+            Debug.Log(json);
+            JsonUtility.FromJsonOverwrite(json, hints);
+        }
+
+        Debug.Log(JsonUtility.ToJson(hints));
+    }
+
+
+    string ReadFromFile(string path) {
+        var filePath = path.Replace(".json", "");
+        TextAsset textFile = Resources.Load(filePath) as TextAsset;
+        return textFile.text;
+    }
+    string CreateDemoHints()
+    {
+        var hint = new Hint();
+        hint.duration = 0.0f;
+        hint.text = "Enter your Hint here!";
+        hint.title = "Provide title!";
+        hints.items.Add(hint);
+        hints.items.Add(hint);
+
+        return JsonUtility.ToJson(hints, true);
+    }
+
+    void WriteToFile(string text, string path)
+    {
+        using (StreamWriter file = File.CreateText(path))
+        {
+            file.WriteAsync(text);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class Hints {
+    public List<Hint> items;
+
+    public Hints() {
+        items = new List<Hint>();
+    }
+}
+
+[System.Serializable]
+public class Hint
+{
+    public string title;
+    public string text;
+    public float duration;
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,29 +6,29 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-   public GameObject loadingScreen;
-   public Slider slider;
-   public Text progressText;
-      
-   public void LoadScene(int sceneIndex)
-   {
-      StartCoroutine(LoadAsynchronously(sceneIndex));
-   }
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
 
-   IEnumerator LoadAsynchronously(int sceneIndex)
-   {
-      AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
 
-      loadingScreen.SetActive(true);
-      
-      while (!operation.isDone)
-      {
-         float progress = Mathf.Clamp01(operation.progress / .9f);
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-         progressText.text = ((int) (progress * 100)).ToString() + " %";
-         slider.value = progress;
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            progressText.text = ((int)(progress * 100)).ToString() + " %";
+            slider.value = progress;
 
             yield return null;
-      }
-   }
+        }
+    }
 }
